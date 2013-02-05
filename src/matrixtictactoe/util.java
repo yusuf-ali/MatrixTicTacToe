505cd0a4ce7df5e04bd0e8b042817f17fe4bc9f3
yusuf-ali <yusuf.ali@hotmail.ca>
2013-02-04 22:49:05 -0500

    Re-Wrote util folder
    - added game_done function
    - these functions still need to be written

--- src/matrixtictactoe/util.java
+++ src/matrixtictactoe/util.java
@@ -1,5 +1,6 @@
 
 package matrixtictactoe;
+import matrixtictactoe.board.board;
 import matrixtictactoe.board.tic;
 
 /*
@@ -8,20 +9,30 @@ import matrixtictactoe.board.tic;
  */
 public class util{
 
-/*  1) this should dictate if a board is won 
+/*  
+ *     1) this should dictate if a board is won 
  *     returns the int of player that wins
  */
-    public int game_done(tic board){
+    public int tic_win(tic board){
         
         
         return 0;   
     }
 
-/*  2) determins if a possible win can happen 
+/*  
+ *     2) determins if a possible win can happen 
  *     returns int of player with possible win 
  */
     public int poss_win(tic board){
         
+        
+        return 0;
+    }
+    
+ /*  
+  * 3) determine if the game is over, the whole board
+  */
+    public int game_done(board game){
         
         return 0;
     }
