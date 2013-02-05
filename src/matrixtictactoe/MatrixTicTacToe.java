427d67327ce9b9260b5cfe0f9544ab590907ad7f
yusuf-ali <yusuf.ali@hotmail.ca>
2013-02-04 20:53:03 -0500

    scew default comments

--- src/matrixtictactoe/MatrixTicTacToe.java
+++ src/matrixtictactoe/MatrixTicTacToe.java
@@ -1,19 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package matrixtictactoe;
 
-/**
- *
- * @author yusufali
- */
+
 public class MatrixTicTacToe {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
