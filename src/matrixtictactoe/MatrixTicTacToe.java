4fcebfb5c5315bd32a299bbae800adc57831a6ae
yusuf-ali <yusuf.ali@hotmail.ca>
2013-02-06 07:20:55 -0500

    ..

--- src/matrixtictactoe/MatrixTicTacToe.java
+++ src/matrixtictactoe/MatrixTicTacToe.java
@@ -5,7 +5,11 @@ package matrixtictactoe;
 public class MatrixTicTacToe {
 
     public static void main(String[] args) {
-       
+     
+        int loc[] = {0,0,0,0};
+        
+        game newGame = new game(loc);
+        
         
     }
 }
--- src/matrixtictactoe/board/tic.java
+++ src/matrixtictactoe/board/tic.java
@@ -9,23 +9,32 @@ package matrixtictactoe.board;
 
 public class tic {
     
-    public tic(){}
-    
-    /*
+     /*
      * X will be represented as 1
      * O will be represented as 2
      * a 0 represents empty spot
      */
-    private int[][] tic = new int[3][3];
+    
+    private int[][] t = new int[3][3];
+    
+    public tic(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                t[i][j] = 0;
+            }
+        }
+    }
+    
+
     
     /* sets the value of the specified co-ordinate */
     public void setState(int x,int y, int value){
-        tic[x][y] = value;
+        //tic[x][y] = value;
     }
     
     /* returns the value of the specified co-ordinate */
     public int getState(int x, int y){
-        return tic[x][y];
+        return t[x][y];
     }
     
 }
