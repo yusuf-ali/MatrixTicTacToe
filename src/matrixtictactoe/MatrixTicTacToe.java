8b57ddbe5cb8c781f8a528d5887f656cb5f61aef
yusuf-ali <yusuf.ali@hotmail.ca>
2013-02-07 09:50:32 -0500

    stupid .DS_Store

--- src/matrixtictactoe/MatrixTicTacToe.java
+++ src/matrixtictactoe/MatrixTicTacToe.java
@@ -15,16 +15,26 @@ public class MatrixTicTacToe {
         int lo2[] = {1,1,0,0};
         int lo3[] = {0,0,1,0};
         int lo4[] = {1,0,0,0};
-        //int lo5[] = {0,0,1,2};
+        int lo5[] = {0,0,2,1};
+        int lo6[] = {2,1,0,0};
+        int lo7[] = {0,0,2,0};
         
         game newGame = new game(lo0);
+        newGame.makeTurn(lo1);
+        newGame.makeTurn(lo2);
+        newGame.makeTurn(lo3);
+        newGame.makeTurn(lo4);
+        
         //newGame.makeTurn(lo5);
+        //newGame.makeTurn(lo6);
+        //newGame.makeTurn(lo7);
         
         engine eng = new engine(newGame);
-        game[] list = eng.getBoards(newGame);
         
-        System.out.println(list[0]);
-        System.out.println(list[3]);
+        int[] j = eng.determinemove(newGame,4);
+        
+        System.out.println(newGame.getState() + "\n");
+        System.out.println(j[0] + "" + j[1] + "" + j[2] + "" + j[3]);
      
     }
 }
