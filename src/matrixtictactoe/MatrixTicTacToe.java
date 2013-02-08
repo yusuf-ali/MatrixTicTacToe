e5796eac32831cd0d2ccafeba068cb7c6e18fac6
yusuf-ali <yusuf.ali@hotmail.ca>
2013-02-07 22:18:32 -0500

    Tested Engine; works up to depth 2
    - still need to spot the error!

--- src/matrixtictactoe/MatrixTicTacToe.java
+++ src/matrixtictactoe/MatrixTicTacToe.java
@@ -16,9 +16,6 @@ public class MatrixTicTacToe {
         int lo2[] = {1,1,0,0};
         int lo3[] = {0,0,1,0};
         int lo4[] = {1,0,0,0};
-        int lo5[] = {0,0,2,1};
-        int lo6[] = {2,1,0,0};
-        int lo7[] = {0,0,2,0};
         
         game newGame = new game(lo0);
         newGame.makeTurn(lo1);
@@ -26,30 +23,9 @@ public class MatrixTicTacToe {
         newGame.makeTurn(lo3);
         newGame.makeTurn(lo4);
         
-        //newGame.makeTurn(lo5);
-        //newGame.makeTurn(lo6);
-        //newGame.makeTurn(lo7);
-        
         engine eng = new engine(newGame);
-        game[] states = eng.getBoards(newGame);
-        
-        //int[] j = {0,0}; int x = 3;
-        //tic b = states[x].getBoard().getState(j);
-        //lol.printTic(b);
-        //System.out.println(states[x].getState());
-        
-        int[] f = eng.determinemove(newGame, 3);
+        int[] f = eng.determinemove(newGame, 2);
         System.out.println(f[0] + "" + f[1] + "" + f[2] + "" + f[3]);
-        
-        //System.out.println(states[x].getTurn());
-        //System.out.println(eng.heuristic(states[x]));
-        
-        //System.out.println(b.getState(0,1));
-     
     }
-    
-    
-    
-    
     
 }
